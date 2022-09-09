54289790cbcb0bae7d248e92df5a9188524b2aee
silentwilful <jymoesh@gmail.com>
2022-09-09 14:30:58 -0700

    all single digit numbers of base

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - all single digit numbers of base
+ *
+ *Return: Always 0
+ **/
+
+int main(void)
+{
+	for (i = '0'; i <= '9'; i++)
+
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
