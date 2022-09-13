406079b2e1bdb9505d9452b9a3a69f116e306a40
silentwilful <jymoesh@gmail.com>
2022-09-13 12:17:46 -0700

    checking alphabet charcters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
+ *
  *Description: int _isalpha check alphabet character
+ *character to check
  *
- *
+ *Return: 0
  */
-
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
