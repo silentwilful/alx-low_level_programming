3dc3f3b4e821c2aecaf1034065cc8d2981de7e00
silentwilful <jymoesh@gmail.com>
2022-09-13 06:58:39 -0700

    master 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#nclude "main.h"
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
