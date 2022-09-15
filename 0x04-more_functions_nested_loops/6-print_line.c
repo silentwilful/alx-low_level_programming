8951af2e79a368745f5cb1f4c86bf090096541cd
silentwilful <jymoesh@gmail.com>
2022-09-15 06:04:49 -0700

    fuction that prints a straight line in the line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_line - straight line
+ *Description: character n print
+ *@n: integer
+ *Return: void
+ */
+void print_line(int n)
+{
+
+	while (n-- > 0)
+
+	{
+
+		_putchar('_');
+
+	}
+
+	_putchar('\n');
+}
