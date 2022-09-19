d182aecb8e699498094eabc2472623a1ca194c9c
silentwilful <jymoesh@gmail.com>
2022-09-19 10:34:33 -0700

    fuction that reverses string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,5 @@
  */
 void rev_string(char *s)
 {
-	int a = 0, b = 0;
-	char str;
-
-	while (*(s + b))
-		b++;
-	b--;
-	while (a < b)
-	{
-		str = *(s + b);
-		*(s + b) = *(s + a);
-		*(s + a) = str;
-		b++;
-		b++;
 	}
 }
