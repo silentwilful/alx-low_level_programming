f3f85589dad4d5b7785a051d276e718d4c6ce8c6
silentwilful <jymoesh@gmail.com>
2022-09-19 12:56:16 -0700

    functions that prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
 #include "main.h"
 /**
  *_puts - print stdout string
- *@*str: printing out a string
+ *@str: printing out a string
  *Description: string print
  *Return: void
  */
 void _puts(char *str)
 {
-	while (*str)
-	_putchar(*str++);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
 
 	_putchar('\n');
 }
