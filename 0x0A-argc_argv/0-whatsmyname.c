85eebe3c8336af23ccf39366a84134ed852dc7cc
silentwilful <jymoesh@gmail.com>
2022-09-30 04:43:56 -0700

    Argumentive print

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - print name
+ *@argc: argument count
+ *@argv: array with command line
+ *
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
